6fbf226b78c822a7254dfb2c4855ff5fd42c9666
gangslee <grandnet1225@gmail.com>
2020-11-19 21:31:58 +0900

    Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -26,7 +26,7 @@ export const getById = (id) => {
 export const deleteMovie = (id) => {
   const cleanedMovies = movies.filter((movie) => movie.id !== id);
   if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
+    movies = cleanedMovies;
     return true;
   } else {
     return false;
