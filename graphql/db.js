f7c615a62e0d2dba3242cb54d1de7cd5e15cabfa
gangslee <grandnet1225@gmail.com>
2020-11-19 21:15:14 +0900

    Define Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,34 @@
-export const people = [
+let movies = [
   {
-    name: 'gangslee',
-    age: '26',
-    gender: 'male',
-    id: '1',
+    id: 0,
+    name: 'Star Wars - The new one',
+    score: 1,
   },
   {
-    name: 'gangslee222',
-    age: '25',
-    gender: 'female',
-    id: '2',
+    id: 1,
+    name: 'Avengers - The new one',
+    score: 8,
   },
   {
-    name: 'gangslee333',
-    age: '23',
-    gender: ' male',
-    id: '3',
+    id: 2,
+    name: 'The Godfather I',
+    score: 99,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movie = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
