ee9d6ee323feb3fd736172e91d256c2f16745e02
gangslee <grandnet1225@gmail.com>
2020-11-21 00:40:28 +0900

    Repair db & resolvers

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import { GraphQLServer } from 'graphql-yoga';
-import resolvers from './graphql/resolvers';
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: 'graphql/schema.graphql',
+  typeDefs: `type Movie {
+    id: Int!
+    title: String!
+    rating: Float
+    description_full: String
+    language: String
+    medium_cover_image: String
+    genres: [String]
+  }
+  
+  type Query {
+    movies(limit: Int, rating: Float): [Movie]!
+    movie(id: Int!): Movie
+    suggestions(id: Int!): [Movie]!
+  }
+  `,
   resolvers,
 });
 
-server.start(() => console.log('GraphQL Server Start'));
+server.start(() => console.log("Graphql Server Running"));
